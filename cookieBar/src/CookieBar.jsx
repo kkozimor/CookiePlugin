8479124f0eccfd84aa7bdff1a09cd519d6e99376
Kacper Kozimor <kkozimor@objectivity.co.uk>
2020-09-10 11:11:48 +0200

    added dynamic link to cookie info

--- cookieBar/src/CookieBar.jsx
+++ cookieBar/src/CookieBar.jsx
@@ -5,18 +5,25 @@ import { Component, createElement } from "react";
 import "./ui/CookieBar.css";
 
 export default class CookieBar extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
-        return <CookieConsent
-        location="bottom"
-        buttonText="Sure man!!"
-        cookieName="myAwesomeCookieName2"
-        style={{ background: "#2B373B" }}
-        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-        expires={150}
-      >
-        This website uses cookies to enhance the user experience.{" "}
-        <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span>
-      </CookieConsent> ;
+        return (
+            <CookieConsent
+                location="bottom"
+                buttonText="Sure man!!"
+                cookieName="myAwesomeCookieName2"
+                style={{ background: "#2B373B" }}
+                buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+                expires={150}
+            >
+                This website uses cookies to enhance the user experience.{" "}
+                <span style={{ fontSize: "10px" }}>
+                    This bit of text is smaller :O <a href="https://www.google.com"> {this.props.sampleText} </a>{" "}
+                </span>{" "}
+            </CookieConsent>
+        );
     }
 }
-
